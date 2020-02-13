7d9223ff08e73947ed2c7fb9452268a96f690b1f
bluet-it <vince@turcsanyivince.hu>
2020-02-13 18:42:53 +0100

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                bool done = true;
+                for (int j = 2; j < i-1; j++)
+                {
+                    if (i%j == 0)
+                    {
+                        done = false;
+                    }
+                }
+                if (done)
+                {
+                    Console.Write(i.ToString() + " ");
+                }
+            }
+            while (true){}
+        }
+    }
+}
